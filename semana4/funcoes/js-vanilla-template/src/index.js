4cef0db31b525b0f17b6ceb3770ce2227ca58463
Welington-Silva <welingtonsilva966@gmail.com>
2020-10-05 16:54:07 -0300

    funçoes-aula13

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,45 @@
+//EXERCICIO1//
+
+//1- VAI SER IMPRESSO O VALOR DO RESULTADO DAS MULTIPLICAÇÕES DAS VARIAVEIS 2,10 *5//
+//2-SIMPLESMENTE O RESULTADO NÃO SERIA MOSTRADO AO USUARIO E FICARIA SOMENTE GUARDADO DENTRO DE UMA VÁRIAVEL//
+
+//EXERCICIO2//
+
+//1- NO CONSOLE SERÃO IMPRESSOS OS NOMES 'DARVAS','CAIO' SOMENTE'//
+//2- ASSIM O NOME DE 'AMANDA' E 'CAIO' SERIAM IMPRESSOS NO CONSOLE//
+
+//EXERCICIO3//
+
+// O QUE A FUNÇÃO FAZ É BASICAMENTE VERIFICAR SE O RESTO DO VALOR DE 'X' É IGUAL A 0 E SE FOR, O VALOR DE X SERÁ MULTIPLICADO POR ELE MESMO E INCREMENTADO AO ARRAY FINAL//
+// O NOME DA FUNÇÃO PODE SER CHAMADO DE NUMEROS PARES//
+
+//EXERCICIO4//
+
+function sobreMim ()
+console.log ("Eu sou o welington, tenho 18 anos e estudo programação full-stack na labenu")
+
+function sobre (nome,idade,cidade,estudo)
+sobre = prompt
+console.log ("Eu sou",nome,"tenho",idade,"anos", "moro", cidade,"em",estudo,"e",Boolean,"estudante");
+
+//EXERCICIO5//
+
+Function valores (prompt(num1,num2))
+soma = num1+num2
+return soma
+console.log (soma);
+
+function valores (valor1,valor2)
+if (valor1>=valor2)
+return (Boolean)
+
+
+let NomedoUser =(prompt()
+
+let repetição= function(array) {
+	for (let i= 0; i <= 10; i++) {
+		console.log(array[i])
+	}
+}
+
+//EXERCICIO6//
